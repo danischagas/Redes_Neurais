5b9c18b9ce7c3730586aae7666d531e0f0649828
Danielle dos Santos Chagas <danielle220038@ilum.cnpem.br>
2023-03-09 17:44:05 -0300

    Primeira Aula

--- AlgoritmosGeneticos/funcoes.py
+++ AlgoritmosGeneticos/funcoes.py
@@ -1,1 +1,10 @@
- 
+def funcao_objetivo_cb(individuo):
+    """ computa a função objetivo no problema das caixas binárias.
+    
+    Args: 
+        Individuo: lista contendo os genes das caixas binárias
+
+    Return:
+        Um valor representando a soma dos genes dos indivíduos.
+    """
+    return sum(individuo)
